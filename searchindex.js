aa08cfe7f626f4cf2da973f4806e369716fd08e7
Neogennium <neogennium@gmx.com>
2021-02-09 14:35:42 -0300

    1

---
+++ searchindex.js
@@ -0,0 +1,18 @@
+// search index for WYSIWYG Web Builder
+var database_length = 0;
+
+function SearchPage(url, title, keywords, description)
+{
+   this.url = url;
+   this.title = title;
+   this.keywords = keywords;
+   this.description = description;
+   return this;
+}
+
+function SearchDatabase()
+{
+   database_length = 0;
+   this[database_length++] = new SearchPage("index.html", "Untitled Page", "untitled page business companyyour company slogan here our customers consulting home about nbsp us services support contact welcome to website lorem ipsum dolor sit amet consectetur adipiscing elit integer nec odio praesent libero sed cursus ante dapibus diam nisi nulla quis sem at nibh elementum imperdiet duis sagittis mauris fusce tellus augue semper porta massa vestibulum lacinia arcu eget analysis class aptent taciti sociosqu ad litora torquent per conubia nostra inceptos himenaeos curabitur sodales ligula in copyright by quot your name all rights reserved mail yourname domain com follow ", "");
+   return this;
+}
