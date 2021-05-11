c031da2dc9496fc087c1130ff01504e20afc781b
Neogennium <neogennium@gmx.com>
2021-05-11 11:06:05 -0300

    e

--- index.js
+++ index.js
@@ -32,4 +32,13 @@
                      text = text + now.getFullYear() + ", Neogennium   ·  All rights reserved  ·";
                      var copyrightnotice = document.getElementById('copyrightnotice');
                      copyrightnotice.innerHTML = text;
-                  
+                     
+   var sc_project=10313336; 
+   var sc_invisible=1; 
+   var sc_security="904f5284"; 
+   var scJsHost = (("https:" == document.location.protocol) ?
+   "https://secure." : "http://www.");
+   document.write("<sc"+"ript type='text/javascript' src='" +
+   scJsHost+
+   "statcounter.com/counter/counter.js'></"+"script>");
+   
