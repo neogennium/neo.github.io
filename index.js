f4b0117fc61f1a720252c80df96fa0a4e1c3a113
Neogennium <neogennium@gmx.com>
2021-05-11 12:25:25 -0300

    4

--- index.js
+++ index.js
@@ -10,3 +10,13 @@
          }
       });
    });
+   
+   var sc_project=10313336; 
+   var sc_invisible=1; 
+   var sc_security="904f5284"; 
+   var scJsHost = (("https:" == document.location.protocol) ?
+   "https://secure." : "http://www.");
+   document.write("<sc"+"ript type='text/javascript' src='" +
+   scJsHost+
+   "statcounter.com/counter/counter.js'></"+"script>");
+   
