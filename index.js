7f0f6031aa4af694668f7b583c545923eb8c8189
Neogennium <neogennium@gmx.com>
2021-05-11 10:40:24 -0300

    u

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
    
+   var sc_project=10313336; 
+   var sc_invisible=1; 
+   var sc_security="904f5284"; 
+   var scJsHost = (("https:" == document.location.protocol) ?
+   "https://secure." : "http://www.");
+   document.write("<sc"+"ript type='text/javascript' src='" +
+   scJsHost+
+   "statcounter.com/counter/counter.js'></"+"script>");
+      
    $(document).ready(function()
    {
       $('#ThemeableMenu1 .dropdown-toggle').dropdown({popperConfig:{placement:'bottom-start',modifiers:{computeStyle:{gpuAcceleration:false}}}});
@@ -32,13 +41,4 @@
                      text = text + now.getFullYear() + ", Neogennium   ·  All rights reserved  ·";
                      var copyrightnotice = document.getElementById('copyrightnotice');
                      copyrightnotice.innerHTML = text;
-                        
-      var sc_project=10313336; 
-      var sc_invisible=1; 
-      var sc_security="904f5284"; 
-      var scJsHost = (("https:" == document.location.protocol) ?
-      "https://secure." : "http://www.");
-      document.write("<sc"+"ript type='text/javascript' src='" +
-      scJsHost+
-      "statcounter.com/counter/counter.js'></"+"script>");
-      
+                  
