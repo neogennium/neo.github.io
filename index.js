9bea23b33cffa35bdc9442c1e8899e5331d860f8
Neogennium <neogennium@gmx.com>
2021-05-10 22:25:37 -0300

    ee

---
+++ index.js
@@ -0,0 +1,24 @@
+   
+   $(document).ready(function()
+   {
+      $('#ThemeableMenu1 .dropdown-toggle').dropdown({popperConfig:{placement:'bottom-start',modifiers:{computeStyle:{gpuAcceleration:false}}}});
+      $(document).on('click','.ThemeableMenu1-navbar-collapse.show',function(e)
+      {
+         if ($(e.target).is('a') && ($(e.target).attr('class') != 'dropdown-toggle')) 
+         {
+            $(this).collapse('hide');
+         }
+      });
+   });
+      
+         var now = new Date();
+         var startYear = "2001";
+         var text =  "Copyright &copy; ";
+         if (startYear != '')
+         {
+            text = text + startYear + "-";
+         }
+         text = text + now.getFullYear() + ", Neogennium   ·  All rights reserved  ·";
+         var copyrightnotice = document.getElementById('copyrightnotice');
+         copyrightnotice.innerHTML = text;
+      
